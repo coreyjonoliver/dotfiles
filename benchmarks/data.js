8bbf480592e866db20fef8a600772b16f9624c73
github-action-benchmark <github@users.noreply.github.com>
2026-03-09 03:04:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 483cf0db593a499a62b0a0e9128c622acdf04ffd

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773025442558,
+  "repoUrl": "https://github.com/coreyjonoliver/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "483cf0db593a499a62b0a0e9128c622acdf04ffd",
+          "message": "Merge pull request #1 from coreyjonoliver/remove-components\n\nRemove COMPONENTS.md",
+          "timestamp": "2026-03-08T22:03:42-05:00",
+          "tree_id": "de0da7d286f76508fe85f4b1cf1b83a48798d52b",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/483cf0db593a499a62b0a0e9128c622acdf04ffd"
+        },
+        "date": 1773025441147,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh interactive startup",
+            "value": 0.06719906934,
+            "range": "0.0016748720629141977",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      }
+    ]
+  }
+}
