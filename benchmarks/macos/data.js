b3598ab39a37f30308c384b6d70c5f5ccb17083f
github-action-benchmark <github@users.noreply.github.com>
2026-03-13 22:46:59 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b0d8b7792e7e590ccd0741e9d197173ecd16e451

---
+++ benchmarks/macos/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773442019922,
+  "repoUrl": "https://github.com/coreyjonoliver/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b0d8b7792e7e590ccd0741e9d197173ecd16e451",
+          "message": "Merge pull request #5 from coreyjonoliver/add-windows-tests\n\nAdd windows tests",
+          "timestamp": "2026-03-13T17:46:38-05:00",
+          "tree_id": "da82f27f949b85a0c2ac20c8e8e4a0a7c341fa72",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/b0d8b7792e7e590ccd0741e9d197173ecd16e451"
+        },
+        "date": 1773442018962,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh interactive startup",
+            "value": 0.09477502008000002,
+            "range": "0.07973037179921467",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      }
+    ]
+  }
+}
