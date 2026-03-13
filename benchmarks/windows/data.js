9c3f109aa9374b9e3392795c009144548175264c
github-action-benchmark <github@users.noreply.github.com>
2026-03-13 23:44:11 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for efc5d3fa5c077d6c4f12eefc9a3b6287703bd42b

---
+++ benchmarks/windows/data.js
@@ -0,0 +1,70 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773445451014,
+  "repoUrl": "https://github.com/coreyjonoliver/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "efc5d3fa5c077d6c4f12eefc9a3b6287703bd42b",
+          "message": "Merge pull request #6 from coreyjonoliver/fix-windows-tests\n\nFix windows test errors",
+          "timestamp": "2026-03-13T18:43:33-05:00",
+          "tree_id": "650b22cd9ae7291ed15d4d2a1079a325f1a6f994",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/efc5d3fa5c077d6c4f12eefc9a3b6287703bd42b"
+        },
+        "date": 1773445445047,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pwsh startup (NoProfile)",
+            "value": 0.229709088,
+            "range": "0.00423940810649841",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "efc5d3fa5c077d6c4f12eefc9a3b6287703bd42b",
+          "message": "Merge pull request #6 from coreyjonoliver/fix-windows-tests\n\nFix windows test errors",
+          "timestamp": "2026-03-13T18:43:33-05:00",
+          "tree_id": "650b22cd9ae7291ed15d4d2a1079a325f1a6f994",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/efc5d3fa5c077d6c4f12eefc9a3b6287703bd42b"
+        },
+        "date": 1773445445047,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pwsh startup (NoProfile)",
+            "value": 0.229709088,
+            "range": "0.00423940810649841",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      }
+    ]
+  }
+}
