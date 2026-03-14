e7983bab9094c0c5e2c460827b7edc077b7be65f
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 01:33:44 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 88335df497c977c648df636dbeea1016c1c7d021

--- benchmarks/windows/data.js
+++ benchmarks/windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773451186249,
+  "lastUpdate": 1773452024101,
   "repoUrl": "https://github.com/coreyjonoliver/dotfiles",
   "entries": {
     "Benchmark": [
@@ -122,6 +122,37 @@ window.BENCHMARK_DATA = {
             "name": "pwsh startup (NoProfile)",
             "value": 0.237982828,
             "range": "0.00624950014122018",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "88335df497c977c648df636dbeea1016c1c7d021",
+          "message": "Merge pull request #9 from coreyjonoliver/update-readme2\n\nadd badges to headers and platform sections of README",
+          "timestamp": "2026-03-13T20:33:08-05:00",
+          "tree_id": "30e9cb3ddd225508335f6fc49c0950ef2d8f0705",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/88335df497c977c648df636dbeea1016c1c7d021"
+        },
+        "date": 1773452021541,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pwsh startup (NoProfile)",
+            "value": 0.237318588,
+            "range": "0.00329788197848255",
             "unit": "seconds",
             "extra": "hyperfine --warmup 3 --runs 10"
           }
