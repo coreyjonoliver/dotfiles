ae3814419696d195bc6cc52dee256edb19560674
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 01:19:46 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fab19708b379c65ddb87c3c86a3069fdbf317e53

--- benchmarks/windows/data.js
+++ benchmarks/windows/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773450301261,
+  "lastUpdate": 1773451186249,
   "repoUrl": "https://github.com/coreyjonoliver/dotfiles",
   "entries": {
     "Benchmark": [
@@ -91,6 +91,37 @@ window.BENCHMARK_DATA = {
             "name": "pwsh startup (NoProfile)",
             "value": 0.229943066,
             "range": "0.00293881329481439",
+            "unit": "seconds",
+            "extra": "hyperfine --warmup 3 --runs 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "corey.jon.oliver@gmail.com",
+            "name": "Corey Oliver",
+            "username": "coreyjonoliver"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fab19708b379c65ddb87c3c86a3069fdbf317e53",
+          "message": "Merge pull request #8 from coreyjonoliver/update-readme\n\nAdd README banners, badges, and snippet install workflow",
+          "timestamp": "2026-03-13T20:19:11-05:00",
+          "tree_id": "876cfce8c00b5752a86c295bdf6e6f2c75ca3e26",
+          "url": "https://github.com/coreyjonoliver/dotfiles/commit/fab19708b379c65ddb87c3c86a3069fdbf317e53"
+        },
+        "date": 1773451183679,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "pwsh startup (NoProfile)",
+            "value": 0.237982828,
+            "range": "0.00624950014122018",
             "unit": "seconds",
             "extra": "hyperfine --warmup 3 --runs 10"
           }
